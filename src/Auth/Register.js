cd480298ab166c6daffd03e81df4217a0c5079e7
JanetAdedosu <adedosuadeola71@gmail.com>
2024-06-03 03:31:32 +0200

    front end

---
+++ src/Auth/Register.js
@@ -0,0 +1,13 @@
+// import React from 'react'
+// import Layout from "./../../components/Layout/Layout"
+
+// const Register = () => {
+//   return (
+//     <Layout title="Register -Ecommerce App">
+//         <h1>Register Page</h1>
+//     </Layout>
+    
+//   )
+// }
+
+// export default Register
